fcbc8aa93e0c022e7ce510b0dcc2f13f421b390b
chayut <chayuttatai@gmail.com>
2024-07-15 14:42:38 +0700

    add menu-list

---
+++ frenchise2/menu-list.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<?php include 'include/head.php';
+
+if ($langId == 1) {
+    $course = "หลักสูตรที่เปิดสอน";
+    $detail1 = "สปาเพื่อสุขภาพ";
+    $detail2 = "สปาเพื่อความงาม";
+    $detail3 = "สปาขั้นสูง";
+    $allcourse = "หลักสูตรทั้งหมด";
+    $click = "สนใจเรียนคอร์สออนไลน์คลิ๊ก!";
+    $reviwe = "ความคิดเห็นจากผู้เรียน";
+} else {
+    $course = "COURSE";
+    $detail1 = "Health Spa Course";
+    $detail2 = "Beauty Spa Course";
+    $detail3 = "Advanced Spa";
+    $allcourse = "All courses";
+    $click = "สนใจเรียนคอร์สออนไลน์คลิ๊ก!";
+    $reviwe = "REVIEW";
+}
+?>
+
+<body>
+
+
+
+
+
+</body>
+
+<?php include 'include/footer.php'; ?>
+
+
+
+</html>
